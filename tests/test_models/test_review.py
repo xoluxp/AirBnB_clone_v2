ff12f9f5636f32a179982a77d217319b35eba558
xoluxp <xoluxp@gmail.com>
2024-04-09 19:54:30 -0700

    slappy

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """ """
 from tests.test_models.test_base_model import test_basemodel
@@ -27,3 +28,4 @@ class test_review(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.text), str)
+
